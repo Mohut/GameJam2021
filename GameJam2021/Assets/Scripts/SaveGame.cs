508331daaace684763d489a55363508d20f38e9a
BastiFLR <sebastian.falser@ue-germany.de>
2021-01-09 20:18:04 +0100

    [ADD] Endscreen and SaveGame

---
+++ GameJam2021/Assets/Scripts/SaveGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveGame : MonoBehaviour
+{
+    private string key = "convinced";
+    
+    [SerializeField] private int score;
+    
+    private void Update()
+    {
+        PlayerPrefs.SetInt(key, score);
+
+        Debug.Log(PlayerPrefs.GetInt(key).ToString());
+    }
+}

c48e02b8a81afefcf32d08b1d948f9c4e490753c
BastiFLR <sebastian.falser@ue-germany.de>
2021-01-09 18:38:37 +0100

    [INIT] Unity Project

---
+++ GameJam2021/Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private Vector2 movementSpeed;
+    private Rigidbody2D body;
+    private GameObject viewArea;
+    private BoxCollider2D collider;
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        body.velocity = movementSpeed;
+    }
+    
+    //checks collision and inverts the movementSpeed variable so the person moves in the opposite direction
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("Border"))
+        {
+            BorderHit();
+        }
+    }
+
+    private void BorderHit()
+    {
+        movementSpeed = -movementSpeed;
+        //transform.localPosition = -transform.localPosition;
+       
+       
+        body.rotation += 180;
+
+    }
+}

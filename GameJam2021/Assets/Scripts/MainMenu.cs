51b65cf6c30253dd3cf710f92c872e02b0080966
BastiFLR <sebastian.falser@ue-germany.de>
2021-01-10 10:44:34 +0100

    [ADD] Flags Menu + descriptions and Tutorial Screen

--- GameJam2021/Assets/Scripts/MainMenu.cs
+++ GameJam2021/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,12 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("EnemyScene");
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
     }
 
     public void QuitGame()

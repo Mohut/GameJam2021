4ed2f81563b87fb21b95c20eaa7c711eb39df2e6
BastiFLR <sebastian.falser@ue-germany.de>
2021-01-10 10:16:23 +0100

    [ADD] Fixed Bug

--- GameJam2021/Assets/LoadSceneEndscreen.cs
+++ GameJam2021/Assets/LoadSceneEndscreen.cs
@@ -5,13 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneEndscreen : MonoBehaviour
 {
+    [SerializeField] private int score = 0;
+    [SerializeField] private string key = "convinced";
     public void Restart()
     {
         SceneManager.LoadScene("EnemyScene");
+        PlayerPrefs.SetInt(key, score);
     }
 
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        PlayerPrefs.SetInt(key, score);
     }
 }
